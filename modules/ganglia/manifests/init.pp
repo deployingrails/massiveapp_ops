24afe211a3fa2fae5d29852463dec9e1afc6e7fb
Tom Copeland <tom@infoether.com>
2011-03-06 21:50:02 -0800

    symlink in vhost config

--- modules/ganglia/manifests/init.pp
+++ modules/ganglia/manifests/init.pp
@@ -2,7 +2,15 @@ class ganglia {
 
   package {
     ["ganglia-monitor", "ganglia-webfrontend","gmetad"]:
-      ensure => installed
+      ensure  => installed,
+      before  => File["/etc/apache2/conf.d/ganglia.conf"]
+  }
+
+  file {
+    "/etc/apache2/conf.d/ganglia.conf":
+      ensure  => link,
+      target  => "/etc/ganglia-webfrontend/apache.conf",
+      notify  => Service["apache2"]
   }
 
 }
