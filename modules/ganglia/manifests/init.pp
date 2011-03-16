19452858c6fc49fae99e916b7b15d1587baa10f6
Tom Copeland <tom@infoether.com>
2011-03-16 11:59:27 -0700

    Ganglia MySQL monitor

--- modules/ganglia/manifests/init.pp
+++ modules/ganglia/manifests/init.pp
@@ -25,6 +25,24 @@ class ganglia {
       mode    => 644,
       notify  => Service["ganglia-monitor"],
       require => Package["ganglia-monitor"];
+    "/etc/ganglia/gmetric":
+      ensure  => directory,
+      owner   => root,
+      group   => root;
+    "/etc/ganglia/gmetric/ganglia_mysql_stats.pl":
+      source  => "puppet:///modules/ganglia/gmetric/ganglia_mysql_stats.pl",
+      owner   => root,
+      group   => root,
+      mode    => 755,
+      require => File["/etc/ganglia/gmetric"]
+  }
+
+  cron {
+    "ganglia_mysql_stats":
+      user    => root,
+      minute  => "*",
+      command => "/etc/ganglia/gmetric/ganglia_mysql_stats.pl",
+      require => File["/etc/ganglia/gmetric/ganglia_mysql_stats.pl"]
   }
 
   service {
