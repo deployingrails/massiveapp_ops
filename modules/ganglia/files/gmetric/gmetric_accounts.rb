9d7ac10cba3adf2b63280823291c10bdb0acf28f
Tom Copeland <tom@infoether.com>
2011-03-22 23:43:09 -0700

    Added a verbose option

--- modules/ganglia/files/gmetric/gmetric_accounts.rb
+++ modules/ganglia/files/gmetric/gmetric_accounts.rb
@@ -2,11 +2,13 @@
 
 def recently_created_record_count
   cmd = 'mysql -uroot -proot --silent --skip-column-names massiveapp --execute '
-  cmd += '"select count(id) from accounts where created_at > (now() - interval 1 hour)"'
+  cmd += '"select count(id) from accounts where '
+  cmd += 'created_at > (now() - interval 1 hour)"'
   `#{cmd}`.strip.to_i
 end
 
 def publish(count)
+  puts "Publishing accounts = #{count}" if ARGV.include?("-v")
   system("gmetric --name 'accounts' --value #{count} --type uint16")
 end
 
