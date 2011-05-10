656f789f4e49de3d50d7b6b738ee76c9bd922b3d
Tom Copeland <tom@infoether.com>
2011-05-09 19:15:03 -0700

    apache piped log rotation

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -5,6 +5,17 @@ class apache {
       ensure => present
   }
 
+  file {
+    "/etc/apache2/apache2.conf":
+      source  => "puppet:///modules/apache/apache2.conf",
+      owner   => root,
+      group   => root,
+      notify  => Service["apache2"],
+      require => Package["apache2"];
+    "/etc/logrotate.d/apache2":
+      ensure  => absent
+  }
+
   service {
     "apache2":
       ensure      => running,
