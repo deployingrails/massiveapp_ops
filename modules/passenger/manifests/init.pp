8a51d3a8a791ea44e51b90ab5b7c507482a579e4
Tom Copeland <tom.copeland@livingsocial.com>
2011-11-15 23:18:56 -0500

    Need dev module

--- modules/passenger/manifests/init.pp
+++ modules/passenger/manifests/init.pp
@@ -1,5 +1,10 @@
 class passenger {
 
+  package {
+    "apache2-prefork-dev":
+      ensure => present
+  }
+
   exec {
     "/usr/local/bin/gem install passenger -v=3.0.9":
       user    => root,
@@ -9,13 +14,14 @@ class passenger {
       unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/passenger-3.0.9/"
   }
 
-exec {
+  exec {
   "/usr/local/bin/passenger-install-apache2-module --auto":
     user    => root,
     group   => root,
     path    => "/bin:/usr/bin:/usr/local/apache2/bin/",
     alias   => "passenger_apache_module",
     before  => File["passenger_conf"],
+    require => Package["apache2-prefork-dev"],
     unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/\
 passenger-3.0.9/ext/apache2/mod_passenger.so"
 }
