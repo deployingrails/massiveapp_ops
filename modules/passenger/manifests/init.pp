b51951325bc0768775b13064e4bdd6d84e869cb7
Tom Copeland <tom.copeland@livingsocial.com>
2011-11-15 23:05:40 -0500

    Passenger module

---
+++ modules/passenger/manifests/init.pp
@@ -0,0 +1,33 @@
+class passenger {
+
+  exec {
+    "/usr/local/bin/gem install passenger -v=3.0.9":
+      user    => root,
+      group   => root,
+      alias   => "install_passenger",
+      before  => [File["passenger_conf"],Exec["passenger_apache_module"]],
+      unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/passenger-3.0.9/"
+  }
+
+exec {
+  "/usr/local/bin/passenger-install-apache2-module --auto":
+    user    => root,
+    group   => root,
+    path    => "/bin:/usr/bin:/usr/local/apache2/bin/",
+    alias   => "passenger_apache_module",
+    before  => File["passenger_conf"],
+    unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/\
+passenger-3.0.9/ext/apache2/mod_passenger.so"
+}
+
+  file {
+    "/etc/apache2/conf.d/passenger.conf":
+      mode    => 644,
+      owner   => root,
+      group   => root,
+      alias   => "passenger_conf",
+      notify  => Service["apache2"],
+      source  => "puppet:///modules/passenger/passenger.conf"
+  }
+}
+
