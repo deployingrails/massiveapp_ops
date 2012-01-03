c40fad61d4b55c480fc2eb5a43bd8532979c9c17
Tom Copeland <tom.copeland@livingsocial.com>
2012-01-02 22:28:34 -0500

    require prefork before installing gem

--- modules/passenger/manifests/init.pp
+++ modules/passenger/manifests/init.pp
@@ -1,8 +1,8 @@
 class passenger {
 
   package {
-    "apache2-prefork-dev":
-      ensure => present
+     "apache2-prefork-dev":
+       ensure => present
   }
 
   exec {
@@ -10,6 +10,7 @@ class passenger {
       user    => root,
       group   => root,
       alias   => "install_passenger",
+      require => [Package["apache2"],Package["apache2-prefork-dev"]],
       before  => [File["passenger_conf"],Exec["passenger_apache_module"]],
       unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/passenger-3.0.9/"
   }
@@ -21,10 +22,9 @@ class passenger {
     path    => "/bin:/usr/bin:/usr/local/apache2/bin/",
     alias   => "passenger_apache_module",
     before  => File["passenger_conf"],
-    require => Package["apache2-prefork-dev"],
     unless  => "ls /usr/local/lib/ruby/gems/1.9.1/gems/\
 passenger-3.0.9/ext/apache2/mod_passenger.so"
-}
+  }
 
   file {
     "/etc/apache2/conf.d/passenger.conf":
