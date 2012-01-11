916ae85c307d4f464bfa6bb443c8da037ed40ff3
Tom Copeland <tom.copeland@livingsocial.com>
2012-01-11 06:31:03 -0500

    Also set password

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -20,5 +20,12 @@ class mysql {
       subscribe => File["/etc/mysql/my.cnf"]
   }
 
+  exec {
+    "mysql_password":
+      unless  => "mysqladmin -uroot -proot status",
+      command => "mysqladmin -uroot password root",
+      require => Service[mysql]
+  }
+
 }
 
